51a35507b86975c84ef9c6e8d8e84a874138fced
NfoTECH <fortunenwachukwu@gmail.com>
2022-07-31 17:40:16 +0100

    removeClientHandler method added

--- src/main/java/com/fortunate/ClientHandler.java
+++ src/main/java/com/fortunate/ClientHandler.java
@@ -54,4 +54,8 @@ public class ClientHandler implements Runnable{
             }
         }
     }
+    public void removeClientHandler() {
+        clientHandlers.remove(this);
+        broadcastMessage("SERVER: " + clientUsername + " has left the chat!");
+    }
 }
