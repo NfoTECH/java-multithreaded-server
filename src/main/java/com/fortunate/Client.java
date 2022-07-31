01ac76502b6b3f88edd1fda9a3a23dd3b61984cf
NfoTECH <fortunenwachukwu@gmail.com>
2022-07-31 17:56:50 +0100

    client constructor method added

--- src/main/java/com/fortunate/Client.java
+++ src/main/java/com/fortunate/Client.java
@@ -1,4 +1,22 @@
 package com.fortunate;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String username;
+
+    public Client(Socket socket, String username) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.username = username;
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
 }
