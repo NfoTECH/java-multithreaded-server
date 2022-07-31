70fd429e5dd4edf1f997bda65fd76e3002590ee6
NfoTECH <fortunenwachukwu@gmail.com>
2022-07-31 17:29:48 +0100

    clientHandler method add to ClientHandler class

--- src/main/java/com/fortunate/ClientHandler.java
+++ src/main/java/com/fortunate/ClientHandler.java
@@ -17,11 +17,29 @@ public class ClientHandler implements Runnable{
             this.socket = socket;
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        } catch ()
+            this.clientUsername = bufferedReader.readLine();
+            clientHandlers.add(this);       //Add this ClientHandler object/method to clientHandlers Arraylist
+            broadcastMessage("SERVER: " + clientUsername + " has entered the chat!");
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
     }
 
+    // Here we listen for messages on a separate thread(a blocking operation)
     @Override
     public void run() {
+        String messageFromClient;
 
+        while (socket.isConnected()) {
+            try {
+                messageFromClient = bufferedReader.readLine();
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeEveryThing(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
     }
+
+
 }
